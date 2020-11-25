cec44ef45243e3651326a5b66d6da7a1edf04ac0
oumaima <abbaouioumaima1@gmail.com>
2020-11-25 15:33:34 +0100

    back school

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const server = express();
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongo/apinodejs')
+// moongoose.connect('mongodb://localhost:27017/apinodejs');  // Without docker
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
+
+const schoolRoute = require('./api/routes/schoolRoute');
+schoolRoute(server);
+
+
+
+server.listen(port, hostname);
